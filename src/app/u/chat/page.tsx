08b1aec14999bfd3161df2a6bd5892ba44c8e505
EboTheFirst <ebokak5@gmail.com>
2025-01-31 13:54:08 +0000

    Added fullscreen and some chat ui changes

--- src/app/u/chat/page.tsx
+++ src/app/u/chat/page.tsx
@@ -22,6 +22,7 @@ const ObjSelect = dynamic(() => import("@/components/inputs/ObjSelect"), {
 import { BiEdit } from "react-icons/bi";
 import { PiPaperPlaneRightFill } from "react-icons/pi";
 import axios from "axios";
+import { CgMaximize, CgMinimize } from "react-icons/cg";
 
 const SOURCES = {
   database: "https://pkhsns2xwi.execute-api.eu-west-1.amazonaws.com/Prod/query", osis: "https://pkhsns2xwi.execute-api.eu-west-1.amazonaws.com/Prod/qa"
@@ -55,9 +56,10 @@ export default function Chat() {
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState<string>("");
   const [chatID, setChatID] = useState<string>((new Date()).toString());
-  const [chatList, setChatList] = useState<Chat[]>([]);
+  const [chatList, setChatList] = useState<Chat[]>([{ id: chatID, messages: [] }]);
   const [messages, setMessages] = useState<any[]>([]);
-  const [url, setURL] = useState<string>(Object.keys(urlOptions)[0])
+  const [isFullScreenChat, setIsFullScreenChat] = useState(false);
+  const [url, setURL] = useState<string>(Object.keys(urlOptions)[0]);
 
   const getResponse = async (query: string) => {
     setLoading(true);
@@ -115,41 +117,74 @@ export default function Chat() {
     }
   };
 
+  const handleNewChat = () => {
+    const updatedChatList = chatList.map((chat) => {
+      if (chat.id == chatID) {
+        chat.messages = [...JSON.parse(JSON.stringify(messages))]
+      }
+      return chat
+    })
+    const newChatID = (new Date()).toString(); // Generate a new unique chat ID
+    setChatID(newChatID);  // Set the new chat ID first
+    setChatList([...updatedChatList, { id: newChatID, messages: [] }]);  // Add new chat with updated ID
+    setMessages([]);  // Reset messages for the new chat
+  };
+
   return (
     <div className="flex-1 flex gap-10 overflow-y-auto">
       <div className="flex flex-[3] flex-col overflow-y-auto pb-5 pt-[2rem]">
-        <div className="ml-8 flex flex-col gap-5">
-          <div className="flex ml-1">
-            <ObjSelect label="Knowledge base" value={url} options={urlOptions} onChange={(e) => { setURL(e.target.value) }} />
-          </div>
-        </div>
-        <div className="ml-8 pl-1 flex flex-col gap-4 my-10">
+
+        <div className="ml-8 pl-1 flex flex-col gap-4 mt-4 mb-10">
           <div className="font-medium text-[0.6rem] uppercase text-dark dark:text-gray-300">Conversations</div>
           <div className="flex">
-            <div onClick={() => { setChatList([...chatList, { id: chatID, messages: messages }]); setChatID((new Date()).toString()); setMessages([]); }} className={`cursor-pointer border-4 border-gray-700 flex items-center gap-[0.74rem] bg-gray-900 text-xs font-bold px-4 py-3 rounded-lg`}>
-              <div className="text-teal-400">{"Let's have a chat"}</div>
-              <div className={`text-2xl flex text-teal-500 items-center justify-center rounded-xl`}>
+            <div onClick={handleNewChat} className={`cursor-pointer border-4 border-gray-700 flex items-center gap-[0.74rem] hover:tracking-wide duration-300 bg-teal-900 text-xs font-bold px-4 py-3 rounded-lg`}>
+              <div className="text-teal-200">{"Let's have a chat"}</div>
+              <div className={`text-2xl flex text-teal-300 items-center justify-center rounded-xl`}>
                 <BiEdit />
               </div>
             </div>
           </div>
-          {/* <IconTextButton invert alt text="Let's have a chat" icon={BiEdit} /> */}
         </div>
         <div className="text-[0.6rem] uppercase font-medium text-dark dark:text-gray-400 ml-10">History</div>
         <div className={` ml-10 relative flex-1 text-xs shadow rounded overflow-hidden mt-3`}>
           <div className="absolute top-0 w-full h-full overflow-y-auto flex-col">
             {chatList.map((chat, index) => (
-              <div onClick={() => { setChatID(chat.id); setMessages(chat.messages) }} key={index} className="cursor-pointer text-teal-400 font-medium text-[0.8rem] bg-gray-800 rounded-lg tracking-wide shadow py-3 px-3 truncate">{chat.id}</div>
+              <div onClick={() => {
+                const updatedChatList = chatList.map((chat) => {
+                  if (chat.id == chatID) {
+                    chat.messages = [...JSON.parse(JSON.stringify(messages))]
+                  }
+                  return chat
+                })
+                setChatList(updatedChatList)
+                setChatID(chat.id); setMessages(chat.messages)
+              }} key={index}
+                className={`cursor-pointer ${chatID == chat.id ? 'border border-white/20 tracking-wide text-teal-100/70' : 'text-teal-100/50'} text-[0.8rem] shadow py-5 px-3 truncate`}>
+                {chat.id}
+              </div>
             ))
             }
           </div>
         </div>
       </div>
-      <div className="flex-[8] relative flex flex-col light:border dark:border-2 border-gray-300 dark:border-gray-800 rounded-t-xl overflow-hidden shadow dark:bg-[#111111] bg-[#f1f1f1] mr-10">
-        <div className="flex absolute bottom-0 w-full h-full overflow-y-auto overflow-x-hidden flex-col border-x rounded-t-lg dark:border-x-0 dark:border-gray-900 flex-1 pb-4">
+      <div className={`${isFullScreenChat ? 'fixed top-0 left-0 right-0 bottom-0 w-full h-screen' : 'flex-[8] relative'} duration-300  flex flex-col light:border dark:border-2 border-gray-300 dark:border-gray-800 rounded-t-xl overflow-hidden shadow dark:bg-[#111111] bg-[#f1f1f1] mr-10`}>
+        <div className={`flex absolute bottom-0 w-full h-full overflow-y-auto overflow-x-hidden flex-col border-x rounded-t-lg dark:border-x-0 dark:border-gray-900 flex-1 pb-4`}>
+          <div onClick={() => { setIsFullScreenChat((prev) => !prev) }} className="absolute top-5 left-5 w-10 h-10 border-2 border-white/50 hover:text-white/80 cursor-pointer text-white/60 text-lg rounded-lg flex items-center justify-center">
+            {
+              isFullScreenChat ?
+                <CgMaximize /> :
+                <CgMinimize />
+            }
+          </div>
+          <div className="absolute top-5 right-5">
+            <div className="flex gap-4 items-center">
+              <div className="text-white/40 text-[0.8rem]">Knowledge Base</div>
+              <ObjSelect value={url} options={urlOptions} onChange={(e) => { setURL(e.target.value) }} />
+            </div>
+          </div>
           {messages.length === 0 ? (
             <div className="flex-1 flex flex-col items-center justify-center">
-              <Loader animationName="bot" style={{ width: "20%" }} />
+              <Loader animationName="bot" style={{ width: "10rem" }} />
               <div className={`text-sm text-dull dark:text-gray-400 font-bold`}>Hi, how can I help you today?</div>
             </div>
           ) : (
